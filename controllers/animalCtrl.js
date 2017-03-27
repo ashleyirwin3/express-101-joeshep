303c9210b379e0b54b043eb7c65b2673773eb9a4
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-03-27 15:31:38 -0500

    created MVC architecture

---
+++ controllers/animalCtrl.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const { join } = require('path')
+const chickData = require('../models/chicken')
+
+const chickenNames = chickData.map( (chicken) => chicken.name)
+
+module.exports.newPage = (req, res, err) =>
+  res.sendFile(join(__dirname, '../public', `${req.url}.html`))
+
+module.exports.submit = (req, res, err) => {
+  console.log(`submitting a form for ${req.url}`)
+  res.send(`No chickens found, this chicken was served - ${chickenNames.join(' and')}`)
+}
